1f9561408381f823e257f44f7ebfa353ef70f084
Pierson <pro.pierson.alban@gmail.com>
2022-07-01 22:59:38 +0200

    add tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import '../styles/components/Header.css'
 function Header() {
-    return <h1 className='titleClassTest'>My first comp'</h1>
+    return <h1 className="text-3xl font-bold underline">My first comp'</h1>
 }
 
 export default Header
