a2d25d4749fa04548f0ab17c4b071234aac1a520
Pierson <pro.pierson.alban@gmail.com>
2022-07-01 22:45:45 +0200

    add styles and assets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
+import '../styles/components/Header.css'
 function Header() {
-    return <h1>My first comp'</h1>
+    return <h1 className='titleClassTest'>My first comp'</h1>
 }
 
 export default Header
