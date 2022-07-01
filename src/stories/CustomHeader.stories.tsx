391b25cd619baa0bffe8a0b5c66999aa3303302d
Pierson <pro.pierson.alban@gmail.com>
2022-07-01 10:31:47 +0200

    add storybook

---
+++ src/stories/CustomHeader.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Header from '../components/Header';
+
+export default {
+    /* 👇 The title prop is optional.
+    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+    * to learn how to generate automatic titles
+    */
+    title: 'CustomHeader',
+    component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const CustomHeader = () => <Header/>;
+
